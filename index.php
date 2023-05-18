30d132f25b069b15060d52870b919c29fda3c4b1
teodora88 <nikolic8teodora@gmail.com>
2023-05-18 14:15:03 +0200

    Kreiran model za dodavanje proizvoda i js za njega

--- index.php
+++ index.php
@@ -79,7 +79,59 @@ if (isset($_COOKIE["admin"]))
         </select>
     </div>
 
-    <div id="podaciPretraga"style="font-size:18px ; margin-top:-80px" ></div>
+    <div id="podaciPretraga"style="font-size:18px ; margin-top:-50px" ></div>
+    </div>
+
+
+    <div class="modal fade" id="my" role="dialog" >
+        <div class="modal-dialog">
+
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body" style="align-items:center; justify-content: center;" >
+                    <div class="container prijava-form">
+                        <form action="#" method="post" id="dodajForm">
+                            <h3 style="color:white; text-align: center; background-color: #A64B2A; border-radius: 45%; padding: 15px; opacity: 80%; ">Dodaj proizvod:</h3>
+                            <div class="row" >
+                                <div class="col-md-11 ">
+                                    <div class="form-group">
+                                        <label style="color:#A64B2A" for="">Ime proizvoda:</label>
+                                        <input type="text" style="border: 1px solid black" name="imeProizvoda" class="form-control" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label style="color:#A64B2A"for="">Broj proizvoda:</label>
+                                        <input type="text" style="border: 1px solid black" name="brojProizvoda" class="form-control" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label style="color:#A64B2A" for="">Cena proizvoda:</label>
+                                        <input type="text" style="border: 1px solid black" name="cena" class="form-control" />
+                                    </div>
+                                    <div class="form-group">
+                                        <select id="kategorijaId" name="kategorijaId" class="form-control">
+                                            <?php
+                                            $rez = $conn->query("SELECT * from kategorija");
+                                            while ($red = $rez->fetch_array()) {
+                                            ?>
+                                                <option name="value" value="<?php echo $red['kategorijaId'] ?>"> <?php echo $red['imeKategorije'] ?></option>
+                                            <?php  }
+                                            ?>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <button id="btnDodaj" type="submit" class="btn btn-success btn-block" style="background-color: #A64B2A">
+                                            Dodaj novi proizvod</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
     </div>
 
 
@@ -101,5 +153,6 @@ if (isset($_COOKIE["admin"]))
 
     <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
 
 </body>
